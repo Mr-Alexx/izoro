44ab61bd9818fd8dcb212f81c11b1ce0ed7c30a0
alex <719876205@qq.com>
2021-02-27 17:15:53 +0800

    init

---
+++ packages/server/src/app.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/test')
+  getTest(): object {
+    return {
+      code: 1,
+      msg: 'success',
+      data: {
+        test: 1
+      }
+    }
+  }
+}
