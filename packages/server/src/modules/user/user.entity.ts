19bf284392d98da5f7e52d07b85c21689ba9f2b2
alex <719876205@qq.com>
2021-03-06 18:06:59 +0800

    保存代码

---
+++ packages/server/src/modules/user/user.entity.ts
@@ -0,0 +1,39 @@
+import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: Number;
+
+  @Column({ comment: '账号' })
+  account: String;
+
+  @Column({ comment: '密码' })
+  password: String;
+
+  @Column({ comment: '昵称', default: null, length: 20 })
+  nickname: String;
+
+  @Column({ comment: '头像', default: null })
+  avatar: String;
+
+  @Column({ comment: '邮箱', default: null })
+  email: String;
+
+  @Column({ comment: '角色' })
+  role: String;
+
+  @CreateDateColumn({
+    type: 'datetime',
+    comment: '创建时间',
+    name: 'create_at',
+  })
+  create_at: Date;
+
+  @UpdateDateColumn({
+    type: 'datetime',
+    comment: '更新时间',
+    name: 'update_at',
+  })
+  update_at: Date;
+}
