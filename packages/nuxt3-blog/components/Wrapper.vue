033fc1cb1a52843bd75e4e050239a0dc7bcf82e9
alex <719876205@qq.com>
2021-11-10 18:11:43 +0800

    ✨ feat(nuxt3-blog): 修改首页

--- packages/nuxt3-blog/components/Wrapper.vue
+++ packages/nuxt3-blog/components/Wrapper.vue
@@ -1,15 +1,45 @@
 <template>
-  <div class="wrapper">
-    <slot></slot>
+  <div class="wrapper" :class="{ 'has-aside': !!slots.aside }">
+    <template v-if="slots?.aside">
+      <div class="wrapper__content">
+        <slot></slot>
+      </div>
+      <div class="wrapper__aside">
+        <slot name="aside"></slot>
+      </div>
+    </template>
+    <template v-else>
+      <slot></slot>
+    </template>
   </div>
 </template>
 
+<script setup lang="ts">
+import { useSlots } from "vue";
+// defineEmits,defineProps无需导入，直接使用
+const slots = useSlots();
+
+console.log(slots);
+</script>
+
 <style lang="scss" scoped>
-@import '@/styles/var.scss';
-@import '@/styles/mixins.scss';
+@import "@/styles/var.scss";
+@import "@/styles/mixins.scss";
 .wrapper {
   width: $design-width;
   margin: 0 auto;
+
+  &.has-aside {
+    display: flex;
+
+    .wrapper__content {
+      flex: 1;
+      padding-right: 20px;
+    }
+    .wrapper__aside {
+      width: 200px;
+    }
+  }
 
   @include query(sm) {
     width: 100%;
--- packages/nuxt3-blog/pages/index.vue
+++ packages/nuxt3-blog/pages/index.vue
@@ -1,5 +1,31 @@
 <template>
-  <div></div>
+  <Wrapper>
+    <div>
+      <div v-for="item in list" :key="item.id">
+        <div>
+          <router-link :to="`/detail/${item.id}`" target="_blank">{{
+            item.title
+          }}</router-link>
+        </div>
+        <div>
+          <span>置顶</span>
+          <span>标签</span>
+          <!-- 分类 -->
+          <span>{{ item?.category?.name }}</span>
+          <span>{{ item.create_at }}</span>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus, facere ea quibusdam maiores dolorem sit vel dolore,
+            veritatis atque, harum fuga voluptatem quo esse. Sunt iure at
+            officia deleniti voluptatum.
+          </p>
+        </div>
+      </div>
+    </div>
+    <Aside slot="aside" />
+  </Wrapper>
 </template>
 
 <script setup>
@@ -22,15 +48,16 @@ watch(author, () => {
   console.log("author change");
 });
 
-// hooks
-onMounted(() => {
-  console.log(list.value);
-});
-
 // method
 const updateAuthor = () => {
   author.value = !author.value;
 };
+
+// hooks
+onMounted(() => {
+  console.log("data", data.value);
+  console.log(list.value);
+});
 </script>
 
 <style lang="scss" scoped>
