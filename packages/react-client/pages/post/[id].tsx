205a1a24c75fb8919d8e3a542215f2854d1f3d0e
Unknown <unknown@example.com>
2022-03-23 23:30:23 +0800

    ✨ feat: 新增文章详情页

--- packages/react-client/pages/post/[id].tsx
+++ packages/react-client/pages/post/[id].tsx
@@ -1,40 +1,44 @@
-import Layout from '../../components/layout';
-import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
-import Date from '../../components/date';
-import utilStyles from '../../styles/utils.module.css';
-import { GetStaticPaths, GetStaticProps } from 'next';
+import { GetServerSidePropsContext, GetStaticPaths, GetStaticProps } from 'next';
+import { get } from '@/utils/request';
+import { useEffect } from 'react';
+import Wrapper from '@/components/Wrapper';
+import Layout from '@/layouts/DefaultLayout';
 
 export default function Post({ postData }) {
+  useEffect(() => {
+    console.log('postData', postData);
+  }, []);
+
   return (
-    <Layout>
+    <>
       <Head>
         <title>{postData.title}</title>
+        <meta name="title" content={postData.title} />
+        <meta name="keyword" content={postData.seo_keywords} />
+        <meta name="description" content={postData.seo_description} />
+        <link rel="stylesheet" href="/css/code-mirror.css" />
       </Head>
-      <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
-    </Layout>
+      <Wrapper aside={<div>test</div>}>
+        <article>
+          <h1>{postData.title}</h1>
+          <div>{/* <Date dateString={postData.date} /> */}</div>
+          <div className="bytemd-editor" dangerouslySetInnerHTML={{ __html: postData.html }} />
+        </article>
+      </Wrapper>
+    </>
   );
 }
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = getAllPostIds();
-  return {
-    paths,
-    fallback: false,
-  };
-};
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const postData = await getPostData(params.id as string);
-  return {
-    props: {
-      postData,
-    },
-  };
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  try {
+    const postData = await get(`/article/${context.params.id}`);
+    return {
+      props: {
+        postData,
+      },
+    };
+  } catch (err) {
+    // console.error(12, err);
+  }
 };
