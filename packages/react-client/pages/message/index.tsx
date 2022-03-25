1bda97a3781c1e26f5bb43c19964276bd684cba0
alex <719876205@qq.com>
2022-03-25 18:14:59 +0800

    save code

--- packages/react-client/pages/message/index.tsx
+++ packages/react-client/pages/message/index.tsx
@@ -1,5 +1,25 @@
+import Skeleton from '@/components/Skeleton';
 import Wrapper from '@/components/Wrapper';
+import { useEffect, useState } from 'react';
 
 export default function Message() {
-  return <Wrapper>留言板</Wrapper>;
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+    return () => {
+      clearTimeout(timer);
+      timer = null;
+    };
+  }, []);
+
+  return (
+    <Wrapper>
+      <Skeleton loading={loading} paragraph={{ rows: 10, width: '100%' }}>
+        测试占位图loading后展示的内容
+      </Skeleton>
+    </Wrapper>
+  );
 }
